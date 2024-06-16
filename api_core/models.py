34fa636f0d67f5a713b199bba6fb0dd54ef10f40
Nayan Yadav <yadavnayan441@gmail.com>
2024-06-16 17:29:38 +0530

    added null and blank field in model

--- api_core/models.py
+++ api_core/models.py
@@ -20,7 +20,7 @@ class UserManager(BaseUserManager):
 
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, blank=True, null=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
